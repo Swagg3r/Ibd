b33128adbb20bf86aea3c08427ae29cac40ee4f1
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-02-13 17:06:08 +0100

    Delete unused method handleMessage()

--- src/chat/ClientItf.java
+++ src/chat/ClientItf.java
@@ -2,6 +2,5 @@ package chat;
 import java.rmi.*;
 
 public interface ClientItf extends Remote {
-	public String handleMessage(Person p, String m) throws RemoteException;
 	public void getMessage(String message) throws RemoteException;
 }
